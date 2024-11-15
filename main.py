06597824aeae6ef9f9c02c0cd61635a055d95fc1
Nizaminem <nagarizaev1994@gmail.com>
2024-11-15 18:52:57 +0300

    15.11.24

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello, World!")
+
+#1st program
+print(5*(9**0.5))
+
+#2st program
+print(9.99>9.98, 1000!=1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#4th program
+строка=123.456
+float(строка)
+print(123.456*10)
+print(int(1234.56 % 10))
