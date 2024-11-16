6c9c98a05ee631c74bf1b9e42dc8c98dc9112f9a
Nizaminem <nagarizaev1994@gmail.com>
2024-11-16 18:05:23 +0300

    16.11.24
    переменные

--- module_1_3.py
+++ module_1_3.py
@@ -6,3 +6,23 @@ NewAge = Age + 2
 print('NewAge:', NewAge)
 Is_student = True
 print('Is student:', Is_student)
+
+
+#Практическое задание "Переменные"
+
+zadach_vsego =12
+chasov_vsego_potracheno =1.5
+nazvanie_kursa ='Python'
+srednee_vremya_vipolneniya = float(chasov_vsego_potracheno/zadach_vsego)
+
+a = zadach_vsego
+aa = ', всего задач: '
+b = chasov_vsego_potracheno
+bb = ', затрачено часов: '
+c = srednee_vremya_vipolneniya
+cc = ', среднее время выполнения '
+d = nazvanie_kursa
+dd = 'Курс: '
+
+print(dd,d,aa,a,bb,b,cc,c,' часа.', sep='')
+
