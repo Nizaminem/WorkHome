27c4ea7473d3a1015e845f2f0c9cea139f93580a
Nizaminem <nagarizaev1994@gmail.com>
2024-11-20 13:15:09 +0300

    Списки. индекскация и методы списков

---
+++ Food.py
@@ -0,0 +1,9 @@
+food = ["apple", "cocount", "banan"]
+food.append(True)
+print(food)
+food.extend(["string", 2])
+print(food)
+food.remove("apple")
+print(food)
+print("cocount" not in food)
+print(food[0:1:1])
